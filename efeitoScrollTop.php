5e1985d5148600d6b659b6eb962c2cdf54fe599c
HenriqueNeubert <neubert.study@gmail.com>
2022-04-28 20:15:51 -0300

    feat: funcao click scroll (0.3)

--- efeitoScrollTop.php
+++ efeitoScrollTop.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-      <link src="assets/efeitoScrollTop.js">
 </head>
 <body  class="bg-dark">
       <main class="py-5">
@@ -15,9 +14,9 @@
                         <h2 class="text-center text-white fw-bold">
                               EFEITO SCROLL TOP
                         </h2>
-                        <div class="d-flex flex-column justify-content-center mt-5 gap-5">
+                        <div class="d-flex flex-column justify-content-center mt-5">
                               <?php for($i = 0; $i < 10; $i++ ){ ?>
-                                    <div class="text-white w-50 mx-auto mb-2">
+                                    <div class="text-white w-50 mx-auto mb-1" style="text-align: justify;">
                                           <p>
                                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quasi de
                                                 bitis error animi provident ipsum totam architecto sit voluptates nemo ab
@@ -38,5 +37,17 @@
             </section>
       </main>
 </body>
+<script>
+
+      var btn = document.querySelector('#clickTop');
+      btn.addEventListener("click", function(){
+            window.scrollTo({
+                  top: 0,
+                  behavior: 'smooth'
+            });
+      })
+
+</script>
+<link src="./assets/efeitoScrollTop.js">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
