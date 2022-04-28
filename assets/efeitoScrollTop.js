b64b5a893a77c275efe7b4700a7b924629332e2f
Henrique Neubert <neubert.study@gmail.com>
2022-04-28 13:10:36 -0300

    feat: inicio efeito scroll topo com js (0.2)

---
+++ assets/efeitoScrollTop.js
@@ -0,0 +1,13 @@
+/*JAVASCRIPT PURO*/
+console.log('assa');
+
+      // var btn = document.querySelector('#clickTop');
+      document.querySelector('#clickTop').addEventListener("click", function(){
+            console.log('assa');
+            // window.scrollTo({
+            //       top: 0,
+            //       behavior: 'smooth'
+            // })
+      }
+
+/*JAVASCRIPT PURO*/
---
+++ efeitoScrollTop.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link src="assets/efeitoScrollTop.js">
+</head>
+<body  class="bg-dark">
+      <main class="py-5">
+            <section>
+                  <div class="container">
+                        <h2 class="text-center text-white fw-bold">
+                              EFEITO SCROLL TOP
+                        </h2>
+                        <div class="d-flex flex-column justify-content-center mt-5 gap-5">
+                              <?php for($i = 0; $i < 10; $i++ ){ ?>
+                                    <div class="text-white w-50 mx-auto mb-2">
+                                          <p>
+                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quasi de
+                                                bitis error animi provident ipsum totam architecto sit voluptates nemo ab
+                                                excepturi consequatur, aliquam nihil pariatur dolore cum, reprehenderit s
+                                                uscipit.
+                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quasi de
+                                                bitis error animi provident ipsum totam architecto sit voluptates nemo ab
+                                                excepturi consequatur, aliquam nihil pariatur dolore cum, reprehenderit s
+                                                uscipit.
+                                          </p>
+                                    </div>
+                              <?php } ?>
+                              <button class="btn btn-primary align-self-center" id="clickTop">
+                                    Voltar ao Topo
+                              </button>
+                        </div>
+                  </div>
+            </section>
+      </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
