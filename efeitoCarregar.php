785675ed9857a369218c4bff6c5b204fa3f31f99
HenriqueNeubert <neubert.study@gmail.com>
2022-05-08 19:54:17 -0300

    feat: adicionado efeitos de loading (0.8)

---
+++ efeitoCarregar.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="assets/efeitoCarregar.css">
+</head>
+<body  class="bg-dark">
+      <main class="py-5">
+            <section>
+                  <div class="container">
+                        <h2 class="text-center text-white fw-bold">
+                              EFEITO PULSE
+                        </h2>
+                        <div class="d-flex flex-column -justify-content-center mt-5 gap-5">
+                              <div class="load-01">
+                                    <span>CARREGANDO</span>
+                                    <div class="block-item">
+                                          <div class="item"></div>
+                                          <div class="item"></div>
+                                          <div class="item"></div>                                          
+                                    </div>
+                              </div>
+                              <div class="load-02">
+                                    <div class="block-item">
+                                          <div class="item"></div>
+                                          <div class="item"></div>
+                                          <div class="item"></div>                                          
+                                    </div>
+                                    <span>CARREGANDO</span>
+                              </div>
+                              <div class="load-03">
+                                    <div class="item"></div>
+                              </div>
+                              <div class="load-04">
+                                    <div class="item"></div>
+                              </div>
+                              <div class="load-05">
+                                    <div class="item"></div>
+                              </div>
+                              <div class="load-06">
+                                    <div class="item"></div>
+                              </div>
+                              <div class="load-07">
+                                    <div class="item"></div>
+                              </div>
+                        </div>
+                  </div>
+            </section>
+      </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
